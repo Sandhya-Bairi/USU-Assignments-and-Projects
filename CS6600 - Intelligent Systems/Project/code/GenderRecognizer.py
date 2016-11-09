a3c43fea54df1c970b4e543cb3a1b932965404f5
Anuj Khasgiwala <mail2anuj2006@gmail.com>
2016-11-09 10:12:29 -0700

    Data Science and  Intelligent system
    Data Science assignment and Intelligent systems project updated

--- CS6600 - Intelligent Systems/Project/code/GenderRecognizer.py
+++ CS6600 - Intelligent Systems/Project/code/GenderRecognizer.py
@@ -1,4 +1,4 @@
-import pyAudioAnalysis;
+import pyAudioAnalysis
 # import seewave;
 # import fftw;
 # import caTools;
@@ -9,12 +9,16 @@ import pyAudioAnalysis;
 # import rpart;
 # import xgboost;
 # import e1071;
-import scipy.io as io;
+import scipy.io
 
-from pyAudioAnalysis import audioTrainTest as aT
+from pyAudioAnalysis import audioBasicIO
+#from pyAudioAnalysis import audioTrainTest as aT
 from pyAudioAnalysis import audioFeatureExtraction
 
-aT.featureAndTrain(["classifierData/music","classifierData/speech"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmSMtemp", False)
-aT.fileClassification("data/doremi.wav", "svmSMtemp","svm") #first parameter in this method is for location of source wav file. and the next parameter is the trained classifier
-Result:
-(0.0, array([ 0.90156761,  0.09843239]), ['music', 'speech'])
+# aT.featureAndTrain(["classifierData/music","classifierData/speech"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmSMtemp", False) #this code first trains an audio segment classifier, given a set of WAV files stored in folders
+# aT.fileClassification("data/doremi.wav", "svmSMtemp","svm") #first parameter in this method is for location of source wav file. and the next parameter is the trained classifier
+
+[Fs, x] = audioBasicIO.readAudioFile("data/3WORDS.wav");
+F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
+print "Anuj"
+print F.size
