c9baa26e6bd4fb46960097436bab11e9efa0fc94
Anuj Khasgiwala <mail2anuj2006@gmail.com>
2017-02-19 23:31:27 -0700

    Assignments updated

--- CS 6810 - Wavelets and Wavelet Algorithms/Assignment 4/Assignment 4/src/Problem/EdgeDetection.java
+++ CS 6810 - Wavelets and Wavelet Algorithms/Assignment 4/Assignment 4/src/Problem/EdgeDetection.java
@@ -77,35 +77,35 @@ public class EdgeDetection extends ImageManip{
     }
 	
 	public static void markFHWTEdgeHillTopsInRowsandCols(String infile, String outfile){
-        Mat orig = Highgui.imread(IMAGE_SOURCE_DIR + infile);
+        Mat orig = Highgui.imread(IMAGE_SOURCE_DIR+infile);
         if (orig.rows() == 0 || orig.cols() == 0) {
-            throw new IllegalArgumentException("Failed to read " + IMAGE_SOURCE_DIR + infile);
+            throw new IllegalArgumentException("Failed to read " + IMAGE_SOURCE_DIR+infile);
         }
-        int num_rows = orig.rows();
-        int num_cols = orig.cols();
-        int length = Math.max(num_cols, num_rows);
+        PadLocalizer.numOfRows = orig.rows();
+        PadLocalizer.numOfCols = orig.cols();
+        int length = Math.max(PadLocalizer.numOfCols, PadLocalizer.numOfRows);
         length = nearestPowerOfTwo(length);
         Size dimensions = new Size(length,length);
         Imgproc.resize(orig, orig, dimensions);
-        num_rows = orig.rows();
-        num_cols = orig.cols();
-        Mat grayscale = new Mat(num_rows, num_cols, CvType.CV_8UC1);
+        PadLocalizer.numOfRows = orig.rows();
+        PadLocalizer.numOfCols = orig.cols();
+        Mat grayscale = new Mat(PadLocalizer.numOfRows, PadLocalizer.numOfCols, CvType.CV_8UC1);
         Imgproc.cvtColor(orig, grayscale, Imgproc.COLOR_RGB2GRAY);
-        for(int row = 0; row < num_rows; row++){
-            for(int col = 0; col < num_cols; col ++){
+        for(int row = 0; row < PadLocalizer.numOfRows; row++){
+            for(int col = 0; col < PadLocalizer.numOfCols; col ++){
                 orig.put(row, col, NO_EDGE_PIX);
             }
         }
-        double[] fhwt_row_pix = new double[num_rows];
-        for (int row = 0; row < num_rows; row++) {
-            for (int col = 0; col < num_cols; col++) {
+        double[] fhwt_row_pix = new double[PadLocalizer.numOfRows];
+        for (int row = 0; row < PadLocalizer.numOfRows; row++) {
+            for (int col = 0; col < PadLocalizer.numOfCols; col++) {
                 fhwt_row_pix[col] = grayscale.get(row, col)[0];
             }
-            OneDHWT.inPlaceFastHaarWaveletTransformForNumIters(fhwt_row_pix, 1);
-            for (int col = 1; col < num_cols; col += 2) {
+            OneDHWT.inPlaceFastHaarWaveletTransformForNumIters(fhwt_row_pix, 4);
+            for (int col = 1; col < PadLocalizer.numOfCols; col += 2) {
                 if (Math.abs(fhwt_row_pix[col]) >= WAVELET_COEFF_THRESH) {
                     if (fhwt_row_pix[col] < 0) {
-                        orig.put(row, col,     YES_EDGE_PIX);
+                        orig.put(row, col, YES_EDGE_PIX);
                     } else if (fhwt_row_pix[col] > 0) {
                         orig.put(row, col - 1, YES_EDGE_PIX);
                     }
@@ -113,16 +113,16 @@ public class EdgeDetection extends ImageManip{
             }
         }
 
-        double[] FHWT_col_pix = new double[num_cols];
-        for(int col = 0; col < num_cols; col++){
-            for(int row = 0; row < num_rows; row++){
+        double[] FHWT_col_pix = new double[PadLocalizer.numOfCols];
+        for(int col = 0; col < PadLocalizer.numOfCols; col++){
+            for(int row = 0; row < PadLocalizer.numOfRows; row++){
             	FHWT_col_pix[row] = grayscale.get(row, col)[0];
             }
-            OneDHWT.inPlaceFastHaarWaveletTransformForNumIters(FHWT_col_pix, 1);
-            for(int row = 1; row < num_rows; row += 2){
+            OneDHWT.inPlaceFastHaarWaveletTransformForNumIters(FHWT_col_pix, 4);
+            for(int row = 1; row < PadLocalizer.numOfRows; row += 2){
                 if(Math.abs(FHWT_col_pix[row]) >= WAVELET_COEFF_THRESH){
                     if(FHWT_col_pix[row] < 0) {
-                        orig.put(row, col,     YES_EDGE_PIX);
+                        orig.put(row, col, YES_EDGE_PIX);
                     } else if (FHWT_col_pix[row] > 0) {
                         orig.put(row - 1, col, YES_EDGE_PIX);
                     }
---
+++ CS 6810 - Wavelets and Wavelet Algorithms/Assignment 5/Assignment 5/src/Problem2/Function.java
@@ -0,0 +1,11 @@
+package Problem2;
+
+public class Function {
+	public double v(double x) {
+		return 0;
+	}
+
+	public double[] generateRangeInterval(double[] domain_values) {
+		return null;
+	}
+}
